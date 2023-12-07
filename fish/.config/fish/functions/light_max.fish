85a5fd8e66388287a4aa9cabf14e176ab247e6e5
Celio Grand <celiogrand@outlook.com>
2023-12-07 12:12:37 +0800

    More backlight control

---
+++ fish/.config/fish/functions/light_max.fish
@@ -0,0 +1,4 @@
+function light_max --wraps='light -S 100' --description 'alias light_max=light -S 100'
+  light -S 100 $argv
+        
+end
---
+++ fish/.config/fish/functions/light_min.fish
@@ -0,0 +1,4 @@
+function light_min --wraps='light -S 0.02' --description 'alias light_min=light -S 0.02'
+  light -S 0.02 $argv
+        
+end
