10b55ea346882e0cea4e4ba5a7b77c04e8482142
Celio Grand <celiogrand@outlook.com>
2024-08-30 10:57:47 +0800

    alacritty hide cursor when typing

--- hosts/naptop/home.nix
+++ hosts/naptop/home.nix
@@ -14,17 +14,18 @@
       enable = true;
       settings = {
         colors.primary.background = "#002020";
+        env.TERM = "alacritty";
+        font = {
+          size = 11;
+          normal.family = "JetBrainsMonoNLNerdFontMono";
+        };
         keyboard.bindings = [
           {
             key = "F11";
             action = "ToggleFullscreen";
           }
         ];
-        env.TERM = "alacritty";
-        font = {
-          size = 11;
-          normal.family = "JetBrainsMonoNLNerdFontMono";
-        };
+        mouse.hide_when_typing = true;
         window.opacity = 0.8;
       };
     };
