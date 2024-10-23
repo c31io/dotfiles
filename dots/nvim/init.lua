8d8836f9259b124c0aabfc336f3324b528a1d910
Celio Grand <celiogrand@outlook.com>
2024-10-23 17:50:22 +0800

    set vimtex leader as ' '

--- dots/nvim/init.lua
+++ dots/nvim/init.lua
@@ -1,5 +1,6 @@
 vim.g.base46_cache = vim.fn.stdpath("data") .. "/nvchad/base46/"
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -14,6 +15,7 @@ vim.opt.rtp:prepend(lazypath)
 local lazy_config = require("configs.lazy")
 
 -- load plugins
+---@diagnostic disable-next-line: different-requires
 require("lazy").setup({
 	{
 		"NvChad/NvChad",
