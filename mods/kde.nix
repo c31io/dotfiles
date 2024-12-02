929f252f3befecb6322caa2c13c133b8fa063b06
Celio Grand <celiogrand@outlook.com>
2024-12-02 10:19:42 +0800

    update

--- mods/kde.nix
+++ mods/kde.nix
@@ -45,6 +45,7 @@
     noto-fonts
     noto-fonts-cjk-sans
     noto-fonts-emoji
-    (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" ]; } )
+    nerd-fonts.fira-code
+    nerd-fonts.jetbrains-mono
   ];
 }
