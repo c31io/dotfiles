e2350c5843d45c71e91ed8134f3f6dd85fe840a0
Celio Grand <celiogrand@outlook.com>
2024-12-02 11:03:37 +0800

    using local nixpkgs for flake input

--- dots/fish/functions/bsu.fish
+++ dots/fish/functions/bsu.fish
@@ -1,3 +1,12 @@
 function bsu
+  set np ~/repo/nixpkgs
+  if test -d $np
+    cd $np
+    set branch (git rev-parse --abbrev-ref HEAD)
+    if test $branch = "nixos-unstable"
+      git pull --depth=1 upstream nixos-unstable:nixos-unstable
+    end
+    cd -
+  end
   nix flake update --flake ~/dotfiles/hosts/$hostname && bs
 end
--- hosts/naptop/flake.nix
+++ hosts/naptop/flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "path:/home/c31io/repo/nixpkgs";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
