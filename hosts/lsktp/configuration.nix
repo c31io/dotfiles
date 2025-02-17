40056aff0b4a7bd299b12fd52c675730067f13e5
Celio Grand <celiogrand@outlook.com>
2025-02-17 18:48:34 +0800

    update lsktp

--- hosts/lsktp/configuration.nix
+++ hosts/lsktp/configuration.nix
@@ -33,14 +33,12 @@ flake-overlays:
     settings.KbdInteractiveAuthentication = false;
     ports = [ 22222 ];
   };
-  services.printing = {
-    enable = true;
-    drivers = [ pkgs.hplipWithPlugin ];
-  };
   services.smartd.enable = true;
   users.users.c31io.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAqoyPCI1c/Jz+U9khyB4kPaiE0/4kq7ii/2/WHLIojV c31io"
   ];
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.overlays = flake-overlays;
+  environment.systemPackages = with pkgs; [ matlab ];
   system.stateVersion = "24.11";
 }
