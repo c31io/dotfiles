7b25c3257928e7aa3cc4c0a7dce98831b7ba422d
Celio Grand <celiogrand@outlook.com>
2025-03-12 19:47:13 +0800

    update naptop

--- mods/pc.nix
+++ mods/pc.nix
@@ -28,6 +28,8 @@
       "nix-command"
       "flakes"
     ];
+    download-buffer-size = 1000000000;
+    narinfo-cache-negative-ttl = 5;
     substituters = [ "http://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" ];
     trusted-users = [ "c31io" ];
   };
