1a18843d7e5b51d11babdcc0475ce4f142f8f9c8
Celio Grand <celiogrand@outlook.com>
2025-03-20 09:20:07 +0800

    smarter v.fish

--- dots/fish/functions/v.fish
+++ dots/fish/functions/v.fish
@@ -1,3 +1,25 @@
-function v --wraps=nvim --description 'alias v=nvim'
-  nvim $argv; 
+function v
+  if test (count $argv) -eq 1
+
+    if test -d $argv[1]
+      set dir $argv[1]
+      set file ""
+    else if test -f $argv[1]
+      set dir (git -C (dirname $argv[1]) rev-parse --show-toplevel)
+      or nvim $argv && return
+      set file (realpath $argv[1])
+    else
+      nvim $argv && return
+    end
+
+    cd $dir
+    test -f .envrc
+    and eval (direnv export fish)
+    nvim $file
+
+  else
+
+    nvim $argv
+
+  end
 end
