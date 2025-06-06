3e824c957bf022a4b497fe1cbdb4a8c12410a8b9
Celio Grand <celiogrand@outlook.com>
2025-06-07 03:36:27 +0800

    update

--- mods/kde.nix
+++ mods/kde.nix
@@ -8,21 +8,6 @@
 {
   programs.kdeconnect.enable = true;
 
-  networking.firewall = {
-    allowedTCPPortRanges = [
-      {
-        from = 1714;
-        to = 1764;
-      } # KDE Connect
-    ];
-    allowedUDPPortRanges = [
-      {
-        from = 1714;
-        to = 1764;
-      } # KDE Connect
-    ];
-  };
-
   i18n = {
     inputMethod = {
       type = "fcitx5";
@@ -36,20 +21,23 @@
   };
 
   services = {
-    xserver.enable = true;
-    libinput.enable = true;
     displayManager.sddm.enable = true;
     desktopManager.plasma6.enable = true;
-    xserver.desktopManager.xterm.enable = false;
-    xserver.xkb.options = "caps:swapescape";
+    libinput.enable = true;
     printing.enable = true;
+
+    xserver = {
+      enable = true;
+      desktopManager.xterm.enable = false;
+      xkb.options = "caps:swapescape";
+    };
   };
 
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
+
   environment.systemPackages = with pkgs; [
     alacritty
-    kdePackages.krdc
     ungoogled-chromium
   ];
 
