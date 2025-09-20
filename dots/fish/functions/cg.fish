da4c484365c85de1db15003248c814f6c8ac621a
Celio Grand <celiogrand@outlook.com>
2025-09-20 20:31:19 +0800

    safer cg

--- dots/fish/functions/cg.fish
+++ dots/fish/functions/cg.fish
@@ -1,6 +1,14 @@
 function cg
-  # system-wide
-  sudo nix-collect-garbage -d
-  # current user
-  nix-collect-garbage -d
-end
+    # system-wide
+    sudo nix-collect-garbage -d --delete-older-than 60d
+    # current user
+    read -p "collect user garbage? y/N" flag
+    if [[ -z $flag || $flag == "n" || $flag == "N" ]]
+        then
+        echo skipped
+        elif [[ $flag == "y" || $flag == "Y"]]
+        then
+        nix-collect-garbage -d
+    else
+        echo "invalid input. skipped"
+    end
