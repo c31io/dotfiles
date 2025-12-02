b79764f83e603b718df2df4948ed893243418b22
Celio Grand <celiogrand@outlook.com>
2025-12-02 13:37:20 +0800

    add cpu freq cap alias

---
+++ dots/fish/functions/f04.fish
@@ -0,0 +1,3 @@
+function f04 --wraps='sudo cpupower frequency-set -u 0.4GHz' --description 'alias f04=sudo cpupower frequency-set -u 0.4GHz'
+    sudo cpupower frequency-set -u 0.4GHz $argv
+end
---
+++ dots/fish/functions/f40.fish
@@ -0,0 +1,3 @@
+function f40 --wraps='sudo cpupower frequency-set -u 4.0GHz' --description 'alias f40=sudo cpupower frequency-set -u 4.0GHz'
+    sudo cpupower frequency-set -u 4.0GHz $argv
+end
