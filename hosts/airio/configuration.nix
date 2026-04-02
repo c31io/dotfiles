d1f9eff0a2ce6137444dde6fdf7e99139113ad53
Celio Grand <celiogrand@outlook.com>
2026-04-02 16:44:08 +0800

    hmmm

--- hosts/airio/configuration.nix
+++ hosts/airio/configuration.nix
@@ -2,14 +2,23 @@
 
 {
   imports = [ ../../mods/nix.nix ];
+
   networking.hostName = "airio";
+
   users.users.c31io = {
     name = "c31io";
     home = "/Users/c31io";
     shell = pkgs.fish;
   };
 
+  environment.variables.EDITOR = "hx";
+
   programs.fish.enable = true;
   nixpkgs.config.allowUnfree = true;
+
   system.stateVersion = 6;
+  system.keyboard = {
+    enableKeyMapping = true;
+    swapLeftCtrlAndFn = true;
+  };
 }
